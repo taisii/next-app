062a9874e07c4ed4ce40a9395152ba1fd259046f
taisii <taisii2252@gmail.com>
2024-01-05 18:32:12 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { ScoreMap } from '@/components/ScoreMap';
 import { Button } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = async () => {
-  return <Button>hello</Button>;
+  return (
+  <ScoreMap/>);
 };
 
 export default HomePage;
