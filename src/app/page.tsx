cd54e53b49f866ac460ad2037353f97103e4dcd6
taisii <taisii2252@gmail.com>
2024-01-06 17:22:42 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { ScoreMap } from '@/components/ScoreMap';
-import { Button } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = async () => {
   return (
-  <ScoreMap/>);
+  <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTNueyFWZc3Gfwb_GnoCRgpDB9Rd39ZODxkbrBqJQebrKBw2O_kfGy5r7TRi1hFuk0emgFoMpW0fc2N/pubhtml?widget=true&amp;headers=false" height={1000} width={5000}/>);
 };
 
 export default HomePage;
