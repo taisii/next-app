fa33f42628e305debc8c70520ec94e09a026cdaf
taisii <taisii2252@gmail.com>
2024-01-07 00:21:36 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 import { MemberList } from '@/components/MemberList';
 import { SpreadSheet } from '@/components/SpreadSheet';
+import { prisma } from '@/infrastructures/prisma';
 import { Card, CardBody, Text, VStack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = async () => {
+  const data = await prisma.todo.findFirst({
+    where: {
+      id: 2,
+    },
+  });
   return (
     <>
       <VStack>
         <Text fontSize="4xl">What is M.LEAGUE</Text>
         <Text fontSize="sm">いま、最高の個人競技が、最高の団体競技になる。</Text>
+        <Text>{data?.title}</Text>
         <Card>
           <CardBody>
             <MemberList />
