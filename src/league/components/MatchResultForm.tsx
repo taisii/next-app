16be1c10d6ea076de4cd462dc3623013a2537a83
taisii <taisii2252@gmail.com>
2024-01-20 12:10:39 +0900

    .

--- src/league/components/MatchResultForm.tsx
+++ src/league/components/MatchResultForm.tsx
@@ -154,7 +154,7 @@ const MatchResultRow: React.FC<MatchResultRowProps> = ({ index }) => {
             <Controller
               control={control}
               name={`matchResults.${index}.matchPlayerPoints.${fieldindex}.point`}
-              render={({ field }) => <Input size="sm" {...field} />}
+              render={({ field }) => <Input size="xs" {...field} />}
             />
           </Td>
         );
