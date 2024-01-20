d36c4fa571af9480ce466761bfd6db170665b662
taisii <taisii2252@gmail.com>
2024-01-20 12:09:43 +0900

    .

---
+++ src/league/components/CreateLeagueForm.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Text, VStack } from '@chakra-ui/react';
+
+export const CreateLeagueForm: React.FC = () => {
+  return (
+    <Text>
+      <VStack>
+        <Text>mamber</Text>
+      </VStack>
+    </Text>
+  );
+};
