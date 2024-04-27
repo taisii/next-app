8dbca92853a8e7a1b1f6be1bc0b9f423a11fc585
taisii <taisii2252@gmail.com>
2024-04-27 18:00:03 +0900

    不要なファイルを削除

--- src/components/organisms/Providers.tsx
+++ src/components/organisms/Providers.tsx
@@ -3,14 +3,10 @@
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 
-import { theme } from '@/theme';
-
 export const Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <>
-      <CacheProvider>
-        <ChakraProvider theme={theme}>{children}</ChakraProvider>
-      </CacheProvider>
-    </>
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
   );
 };
--- src/league/mutations/createMatchRecords.tsx
+++
@@ -1,7 +0,0 @@
-'use server';
-import { prisma } from '@/infrastructures/prisma';
-
-export const createMatchResult = async (sessionId: number) => {
-  const matchResult = await prisma.matchResult.create({ data: { sessionId } });
-  return matchResult;
-};
