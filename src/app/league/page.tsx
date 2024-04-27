c5bea9ed9491076b4638c461664e616761175833
taisii <taisii2252@gmail.com>
2024-04-27 21:02:04 +0900

    入力フォームのvalidation

--- src/app/league/page.tsx
+++ src/app/league/page.tsx
@@ -1,39 +1,71 @@
 'use client';
 
-import { Box, Button, Input, VStack } from '@chakra-ui/react';
+import { Box, Button, Flex, FormControl, FormErrorMessage, Input, VStack } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import { ChangeEvent, useState } from 'react';
 
 const LeagueInitPage: NextPage = () => {
   const [nameList, setNameList] = useState<String[]>([]);
   const [addingName, setAddingName] = useState('');
+  const [isNameEmptyLabelShown, setIsNameEmptyLabelShown] = useState(false);
+  const [isNameExistLabelShown, setIsNameExistLabelShown] = useState(false);
 
   const handleOnChangeText = (e: ChangeEvent<HTMLInputElement>) => {
     setAddingName(e.target.value);
   };
 
   const handleClickAddButton = () => {
-    setNameList([...nameList, addingName]);
-    setAddingName('');
+    const isNameEmpty = addingName === '';
+    const isNameExist = nameList.includes(addingName);
+    setIsNameEmptyLabelShown(isNameEmpty);
+    setIsNameExistLabelShown(isNameExist);
+    if (!isNameEmpty && !isNameExist) {
+      setNameList([...nameList, addingName]);
+      setAddingName('');
+    }
   };
 
   return (
-    <VStack mt={10}>
-      <Input
-        size="lg"
-        width="90%"
-        placeholder="name"
-        focusBorderColor="yellow.500"
-        value={addingName}
-        onChange={handleOnChangeText}
-      />
-      <Button onClick={handleClickAddButton}>Add</Button>
-      {nameList.map((name, index) => (
-        <Box key={index}>{name}</Box>
-      ))}
-      <Button>Finalise</Button>
+    <VStack mt="3rem">
+      <Flex direction={'column'} width="90%">
+        <FormControl isInvalid={isNameEmptyLabelShown || isNameExistLabelShown} height="5rem">
+          <Input
+            size="lg"
+            placeholder="name"
+            focusBorderColor="yellow.500"
+            value={addingName}
+            onChange={handleOnChangeText}
+          />
+          <NameFormHelperText
+            isNameEmptyLabelShown={isNameEmptyLabelShown}
+            isNameExistLabelShown={isNameExistLabelShown}
+          />
+        </FormControl>
+
+        <Button onClick={handleClickAddButton}>Add</Button>
+        {nameList.map((name, index) => (
+          <Box key={index}>{name}</Box>
+        ))}
+      </Flex>
+      <Button position="fixed" width="90%" mb="2rem" bottom={0}>
+        Finalise
+      </Button>
     </VStack>
   );
+};
+
+const NameFormHelperText = ({
+  isNameEmptyLabelShown,
+  isNameExistLabelShown,
+}: {
+  isNameEmptyLabelShown: boolean;
+  isNameExistLabelShown: boolean;
+}) => {
+  if (isNameEmptyLabelShown) {
+    return <FormErrorMessage>名前を入力してください</FormErrorMessage>;
+  } else if (isNameExistLabelShown) {
+    return <FormErrorMessage>同じ名前は使えません</FormErrorMessage>;
+  }
 };
 
 export default LeagueInitPage;
